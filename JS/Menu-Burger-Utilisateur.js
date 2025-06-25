be5ae7a1a84e3960ce43b2068df94e39fe909aed
Tonio <falvo.tonio@gmail.com>
2025-06-25 14:50:39 +0200

    Commentaires sur ma fonction menu utilisateur

--- JS/Menu-Burger-Utilisateur.js
+++ JS/Menu-Burger-Utilisateur.js
@@ -35,20 +35,26 @@ const MenuUtilisateur = document.querySelector("#menu-utilisateur");
 function AfficherMenuUtilisateur(event)
 {
         event.preventDefault();
-        // annule le click sur le lien (sinon je peux pas y accéder, ca change de page sinon)
-        // Le paramètre (event) est envoyé directement par le addEventListener et contient l'évènement au clique ou au survol 
+        // preventDefault() = Empêche le comportement normal, c'est à dire si mon icone est dans un lien <& href="...3 normalemnt à a changerait de page"
+        // En gros cette ligne nous dit " Non ne change pas de page reste ici"
+
+    
+        // event = C'est un paramètre qui contient les infos sur ce qui s'est passé au clic et au survol, il fait donc
+        // référence au addEventListener
+
     const MenuVisible = MenuUtilisateur.style.transform === "scale(1)";
-    // 
+    // Le "===" est une comparaison stricte : 'est ce que c'est exactement égal ?
+    // En résumé MenuVisible vaut true si le menu est visible sinon il vaut false
 
     if(!MenuVisible)
     {
-        // Si le menu est fermé on va l'ouvrir
+        // si le menu N'EST PAS visible alors :
             MenuUtilisateur.style.transform = "scale(1)";
             MenuUtilisateur.style.opacity = "1";
             MenuUtilisateur.style.visibility = "visible";
     }
     else
-        // Sinon le menu était déjà visible
+        // Sinon le menu est invisible
     {
             MenuUtilisateur.style.transform = "scale(0)";
             MenuUtilisateur.style.opacity = "0";
@@ -62,19 +68,34 @@ export function StartMenuUtilisateur()
 {
     
     if(IconeUtilisateur.classList.contains("connected"))
-        // vérifie si j'ai la class "connected"
+        // est-ce que l'élément a la classe 'connected' ?
+        // on n'active le menu QUE si l'utilisateur est connecté
+
         // j'ajoute les évènements au survol et au clique
     {
         IconeUtilisateur.addEventListener("click", AfficherMenuUtilisateur)
+        // Quand on clique sur l'icône, on exécute la fonction AfficherMenuUtilisateur"
         IconeUtilisateur.addEventListener("mouseenter", AfficherMenuUtilisateur)
+        // "mouseenter" = quand la souris "entre" sur l'élément (survol)
+        // Même fonction appelée = le menu va s'ouvrir/fermer au survol
         MenuUtilisateur.addEventListener("mouseleave", AfficherMenuUtilisateur)
+        // "mouseleave" = quand la souris "quitte" l'élément
+        // Même fonction = le menu va s'ouvrir/fermer quand on quitte le menu
     }
     // Quand je click sur le logo il se passe quelque chose
     // Ici ça sera le fait d'ouvrir et de fermer le menu
 
 }
+/* 
+Pour conclure :
+Mon script fait ceci :
 
+Si l'utilisateur est connecté (classe "connected")
+Au clic sur l'icône → ouvre/ferme le menu
+Au survol de l'icône → ouvre/ferme le menu
+Quand on quitte le menu → ouvre/ferme le menu
 
+ */
 
 
 
